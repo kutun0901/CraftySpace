95d8d0932f84e52175c760ce34c4d35fdee81d82
kutun0901 <tuan.tq2433@gmail.com>
2023-03-23 16:25:27 -0700

    fix bug in shpping cart api route

--- app/api/shopping_cart_routes.py
+++ app/api/shopping_cart_routes.py
@@ -10,7 +10,7 @@ shopping_cart_routes = Blueprint("shoppingCart", __name__)
 @shopping_cart_routes.route('/')
 @login_required
 def get_all_items_in_cart ():
-    items = InCartItem.query.filter(InCartItem.user_id == current_user.id)
+    items = InCartItem.query.filter(InCartItem.user_id == current_user.id).all()
     return [item.to_dict() for item in items]
 
 
@@ -22,36 +22,46 @@ def add_to_cart():
     form["csrf_token"].data = request.cookies["csrf_token"]
 
     if form.validate_on_submit():
-        item = InCartItem(
-            user_id=data["user_id"],
-            product_id=data["product_id"],
-            quantity=data["quantity"],
-        )
-        db.session.add(item)
+        # Check if the item already exists in the cart
+        item = InCartItem.query.filter_by(user_id=data["user_id"], product_id=data["product_id"]).first()
+
+        if item:
+            # If the item already exists, increase the quantity
+            item.quantity += data["quantity"]
+        else:
+            # If the item does not exist, create a new item
+            item = InCartItem(
+                user_id=data["user_id"],
+                product_id=data["product_id"],
+                quantity=data["quantity"],
+            )
+            db.session.add(item)
+
         db.session.commit()
         return item.to_dict()
+    else:
+        return {'errors': validation_errors_to_error_messages(form.errors)}
 
-@shopping_cart_routes.route('/', methods=["PUT"])
+@shopping_cart_routes.route('/<int:id>', methods=["PUT"])
 @login_required
-def edit_cart_item():
+def edit_cart_item(id):
     data = request.get_json()
-
-    item = InCartItem.query.get(data["id"])
+    item = InCartItem.query.get(id)
 
     if item:
-        item.quantity = data["quantity"]
+        item.quantity = data.get("quantity", item.quantity)
         db.session.commit()
 
     return item.to_dict()
 
-@shopping_cart_routes.route('/', methods=["DELETE"])
+@shopping_cart_routes.route('/<int:id>', methods=["DELETE"])
 @login_required
-def remove_item_in_cart():
-    data = request.get_json()
+def remove_item_in_cart(id):
+    item = InCartItem.query.get(id)
 
-    item = InCartItem.query.get(data["id"])
+    if not item:
+        return {"error": "Item not found"}, 404
 
-    if item:
-        db.session.delete(item)
-        db.session.commit()
-        return {"message": "Successfully removed"}
+    db.session.delete(item)
+    db.session.commit()
+    return {"message": "Successfully removed"}
