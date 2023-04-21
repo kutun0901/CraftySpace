85aff4baeab290c038ebc1ee3e3e31e72571e395
kutun0901 <tuan.tq2433@gmail.com>
2023-04-21 15:56:16 -0700

    404 page

---
+++ react-app/src/components/404Page/index.js
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom";
+import "./404Page.css";
+import { NavLink } from "react-router-dom/react-router-dom";
+
+function PageNotFound() {
+    return (
+        <div className="page-not-found-wrapper">
+                <div className="page-not-found-content-div">
+                    <h2>Uh oh!</h2>
+                    <h4>Sorry, the page you were looking for was <strong>not found</strong>.</h4>
+                    <span>
+                        <NavLink>
+                        <h4><i className="fa-solid fa-arrow-left"></i>Go back to CraftySpace.com</h4>
+                        </NavLink>
+                    </span>
+                    <span>
+                        <NavLink>
+                            <h4>Check out our <a href="https://github.com/kutun0901/CraftySpace" rel="noreferrer" target="_blank">Github repository</a> for help.<i className="fa-solid fa-arrow-right"></i></h4>
+                        </NavLink>
+                    </span>
+                </div>
+        </div>
+    )
+}
+
+export default PageNotFound;
