19ca81b1a4c1550ef91b42ea478c19456da69e8b
kutun0901 <tuan.tq2433@gmail.com>
2023-04-24 14:02:29 -0700

    add no result img

--- react-app/src/components/Search/index.js
+++ react-app/src/components/Search/index.js
@@ -1,11 +1,41 @@
 import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
+import { useState, useEffect } from "react";
+import Loading from "../Loading";
+import noResults from "../../asset/-unsuccessful-state-feedback.jpg"
 
 function Search() {
     const resultsObj = useSelector(state => state.search);
     const placeholderImage = 'https://papapita.com/file/2020/02/Image-Coming-Soon-Placeholder.jpg';
     const results = Object.values(resultsObj);
+    const [isLoaded, setIsLoaded] = useState(false);
 
+
+    // Simulate asynchronous data loading with useEffect
+    useEffect(() => {
+        // Simulated API call with setTimeout
+        setTimeout(() => {
+            setIsLoaded(true); // Set isLoaded to true after 1 second
+        }, 1000);
+    }, []);
+
+    // Render loading component while data is being loaded
+    if (!isLoaded) {
+        return <Loading />;
+    }
+
+    // Render custom message when there are no search results
+    if (results.length === 0) {
+        return (
+            <div className="no-results">
+                <img src={noResults} alt="No Results found"></img>
+                <h3 className="shop-ours">Sorry, we couldn't find what you looking for</h3>
+                <p>Try searching for something else instead?</p>
+            </div>
+        );
+    }
+
+    // Render product cards when there are search results
     return (
         <div>
             <h3 className="shop-ours">Your search results:</h3>
@@ -35,7 +65,7 @@ function Search() {
                 ))}
             </div>
         </div>
-    )
+    );
 }
 
 export default Search;
