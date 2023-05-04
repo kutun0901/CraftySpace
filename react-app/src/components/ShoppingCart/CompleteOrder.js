569aebbb8557f7b96d519c457b116028c0225d1e
kutun0901 <tuan.tq2433@gmail.com>
2023-05-04 10:06:58 -0700

    complete order component

--- react-app/src/components/ShoppingCart/CompleteOrder.js
+++ react-app/src/components/ShoppingCart/CompleteOrder.js
@@ -1,7 +1,14 @@
-function CompleteOrder() {
+import React from 'react';
 
-    return (
-        <>
-        </>
-    )
+function CompleteOrder() {
+  return (
+    <div>
+      <h2>Thank you for your order!</h2>
+      <p>Your payment has been processed and your order is being prepared for shipment.</p>
+      <p>We'll send you an email with tracking information as soon as your order ships.</p>
+      <p>Note: This is not a real e-commerce site, so payment has not actually been implemented.</p>
+    </div>
+  );
 }
+
+export default CompleteOrder;
