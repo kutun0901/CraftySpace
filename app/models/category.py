47483a52d2225d7fcd335ed9f90f9b9c58b12051
kutun0901 <tuan.tq2433@gmail.com>
2023-05-15 08:47:20 -0700

    include products in category to dict method

--- app/models/category.py
+++ app/models/category.py
@@ -16,5 +16,6 @@ class Category(db.Model):
     def to_dict(self):
         return {
             "id": self.id,
-            "name": self.name
+            "name": self.name,
+            "products": [product.to_dict() for product in self.products]
         }
